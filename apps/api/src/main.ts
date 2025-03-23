e87bc7d8fdc7cd28e5745d8774202e567c169f6f
Hestice <custillano@gmail.com>
2025-03-23 09:57:36 +0800

    misc: add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,12 +6,21 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('ABC Admin API')
+    .setDescription('API for Animal Bite Clinic Admin App')
+    .setVersion('1.0')
+    .addTag('abc-admin')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8080;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
