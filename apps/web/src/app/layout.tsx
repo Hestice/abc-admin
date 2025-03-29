9bfe7c3052f94f0e58959e973a00071c9f08f0fa
Hestice <custillano@gmail.com>
2025-03-29 22:42:13 +0800

    feat: login page

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="h-screen flex">{children}</body>
     </html>
   );
 }
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import LoginCard from '@/components/login-card';
 
 export default function Index() {
-  return;
+  return (
+    <div className=" w-full flex items-center justify-center h-auto">
+      <LoginCard />
+    </div>
+  );
 }
